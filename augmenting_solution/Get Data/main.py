dae8fd0adf998035cb865ee57466f5f469270aa0
Luke Hennessy <lukehenn@icloud.com>
2024-12-05 13:35:44 -0500

    Changes to graphs

--- augmenting_solution/Get Data/main.py
+++ augmenting_solution/Get Data/main.py
@@ -28,7 +28,7 @@ def parse_input(input_string):
 def main():
     # Read input from stdin
     # input_data = sys.stdin.read()
-    vertices = 35
+    vertices = 15
     input_data = cs412_augment.generate_complete_graph_test_case(vertices, (1.0, 50.0))
 
     # Parse the input graph
@@ -36,12 +36,12 @@ def main():
     
     graph, num_cities = graph, vertices # change this number for vertices
 
-    # # EXACT
-    # best_path, min_cost = cs412_tsp_exact.tsp_exact(graph)
+    # EXACT
+    best_path, min_cost = cs412_tsp_exact.tsp_exact(graph)
 
-    # # Print the output in the required format
-    # print(f"Exact cost: {min_cost:.4f}")
-    # print(" ".join(best_path))
+    # Print the output in the required format
+    print(f"Exact cost: {min_cost:.4f}")
+    print(" ".join(best_path))
 
     # APPROXIMATE
     timeout = 1
